f64e985a8b144e34151a8884c4eb22f82b518542
Joey <legerinvestments@gmail.com>
2020-02-14 11:55:07 -0500

    set up navbar markup

--- src/scripts/modules/NavBar/NavBar.js
+++ src/scripts/modules/NavBar/NavBar.js
@@ -3,8 +3,8 @@ import './styles.scss';
 const NavBar = (() => {
   const setupEventListeners = () => {
     document.body.addEventListener('click', e => {
-      if (e.target.className === 'accordian__title') {
-        toggleAccordian(e.target);
+      if (e.target.className === 'navbar__item') {
+        // do something
       }
     });
   };
@@ -13,7 +13,15 @@ const NavBar = (() => {
     setupEventListeners();
 
     return `
-    <nav>This is the nav bar</nav>`;
+    <nav class='navbar'>
+      <ul class='navbar__container'>
+        <li class='navbar__item'>Products</li>
+        <li class='navbar__item'>How To Use</li>
+        <li class='navbar__item'>Historical Data</li>
+        <li class='navbar__item'>Indicator/EA</li>
+        <li class='navbar__item'>Support</li>
+      </ul>
+    </nav>`;
   };
 
   return render();
