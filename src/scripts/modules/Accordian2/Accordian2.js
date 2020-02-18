48abaf86d8be545d89b6d2258210160a2afe8071
Joey Leger <legerinvestments@gmail.com>
2020-02-18 09:25:58 -0500

    changed accordian to class

---
+++ src/scripts/modules/Accordian2/Accordian2.js
@@ -0,0 +1,40 @@
+import './styles.scss';
+
+export default class Accordian2 {
+  constructor(data) {
+    this.data = data;
+  }
+
+  renderItems() {
+    return this.data
+      .map(
+        elm =>
+          `<li>
+            <span class='accordian__title'>${elm.title}</span>
+            <span class='accordian__body'>${elm.text}</span>
+          </li>`
+      )
+      .join('');
+  }
+
+  toggleItem(target) {
+    target.classList.toggle('accordian__body--show');
+  }
+
+  setupEventListeners() {
+    document.body.addEventListener('click', e => {
+      if (e.target.className === 'accordian__title') {
+        this.toggleItem(e.target.nextElementSibling);
+      }
+    });
+  }
+
+  render() {
+    this.setupEventListeners();
+
+    return `
+    <ul class='accordian__ul'>
+      ${this.renderItems()}
+    </ul>`;
+  }
+}
--- src/scripts/modules/App/App.js
+++ src/scripts/modules/App/App.js
@@ -1,11 +1,16 @@
-import Accordian from '../Accordian/Accordian';
+import Accordian2 from '../Accordian2/Accordian2';
 import TestMod2 from '../TestMod2/TestMod2';
 import NavBar2 from '../NavBar2/NavBar2';
 
 const App = (() => {
-  const navData = ['Products', 'How To Use', 'Historical Data', 'Indicator/EA Support'];
-  const rootNode = document.getElementById('root');
   let _App;
+  const rootNode = document.getElementById('root');
+  const accordianData = [
+    { title: 'This is the first title', text: 'This is some cool text man' },
+    { title: 'This is the second title', text: 'Wow this is very intersting' },
+    { title: 'This is the third title', text: 'Can you believe what youre reading?' }
+  ];
+  const navData = ['Products', 'How To Use', 'Historical Data', 'Indicator/EA Support'];
 
   const appendToDOM = () => {
     _App = document.createElement('div');
@@ -16,7 +21,7 @@ const App = (() => {
     appendToDOM();
     const DOM = `
     ${new NavBar2(navData).render()}
-    ${Accordian}
+    ${new Accordian2(accordianData).render()}
     ${TestMod2}
     `;
     _App.insertAdjacentHTML('beforeend', DOM);
