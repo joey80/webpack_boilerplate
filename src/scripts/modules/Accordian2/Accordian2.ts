c130a681ba7e41211840188ab393e7e816cad1d5
Joey <legerinvestments@gmail.com>
2020-03-09 18:05:07 -0400

    converted Accordian2 to typescript

---
+++ src/scripts/modules/Accordian2/Accordian2.ts
@@ -0,0 +1,67 @@
+import './styles.scss';
+
+interface Accordian2Props {
+  className: string;
+  data: {
+    text: string;
+    title: string;
+  };
+}
+
+export default class Accordian2 {
+  className: string;
+  data: [
+    {
+      text: string;
+      title: string;
+    }
+  ];
+
+  constructor(props: Accordian2Props) {
+    for (let key in props) {
+      this[key] = props[key];
+    }
+  }
+
+  renderItems() {
+    return this.data
+      .map(
+        elm =>
+          `<li class='accordian__li'>
+            <span class='accordian__title'>${elm.title}</span>
+            <span class='accordian__body'>${elm.text}</span>
+          </li>`
+      )
+      .join('');
+  }
+
+  toggleItem(target) {
+    target.classList.toggle('accordian__body--show');
+  }
+
+  setupEventListeners() {
+    document.body.addEventListener('click', e => {
+      const elm = e.target as HTMLElement;
+      if (elm.className === 'accordian__li') {
+        this.toggleItem(elm.lastElementChild);
+      }
+
+      if (elm.className === 'accordian__title') {
+        this.toggleItem(elm.nextElementSibling);
+      }
+
+      if (elm.classList.contains('accordian__body')) {
+        this.toggleItem(e.target);
+      }
+    });
+  }
+
+  render() {
+    this.setupEventListeners();
+
+    return `
+    <ul class='accordian__ul ${this.className}'>
+      ${this.renderItems()}
+    </ul>`;
+  }
+}
