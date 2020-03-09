7598235e8336833e01ac60a21f9f153fe5927e3e
Joey <legerinvestments@gmail.com>
2020-03-09 18:10:25 -0400

    added data export to App

--- src/scripts/modules/App/App.js
+++ src/scripts/modules/App/App.js
@@ -1,25 +1,15 @@
 import Accordian2 from '../Accordian2/Accordian2';
 import TestMod2 from '../TestMod2/TestMod2';
 import NavBar from '../NavBar/NavBar';
+import { Data } from './Data';
 
 const App = (() => {
   const rootNode = document.getElementById('root');
-  const accordianData = [
-    { title: 'This is the first title', text: 'This is some cool text man' },
-    { title: 'This is the second title', text: 'Wow this is very intersting' },
-    { title: 'This is the third title', text: 'Can you believe what youre reading?' }
-  ];
-  const navData = [
-    { title: 'Products', sub: ['one', 'two', 'three'] },
-    { title: 'How To Use' },
-    { title: 'Historical Data', sub: ['four', 'five', 'six'] },
-    { title: 'Indicator/EA Support', sub: ['seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'] }
-  ];
 
   const render = () => {
     const DOM = `
-    ${new NavBar({ data: navData, className: 'demo-navbar' }).render()}
-    ${new Accordian2({ data: accordianData, className: 'demo-accordian' }).render()}
+    ${new NavBar({ data: Data.navData, className: 'demo-navbar' }).render()}
+    ${new Accordian2({ data: Data.accordianData, className: 'demo-accordian' }).render()}
     ${TestMod2}
     `;
     rootNode.insertAdjacentHTML('beforeend', DOM);
---
+++ src/scripts/modules/App/Data.js
@@ -0,0 +1,13 @@
+export const Data = {
+  accordianData: [
+    { title: 'This is the first title', text: 'This is some cool text man' },
+    { title: 'This is the second title', text: 'Wow this is very intersting' },
+    { title: 'This is the third title', text: 'Can you believe what youre reading?' }
+  ],
+  navData: [
+    { title: 'Products', sub: ['one', 'two', 'three'] },
+    { title: 'How To Use' },
+    { title: 'Historical Data', sub: ['four', 'five', 'six'] },
+    { title: 'Indicator/EA Support', sub: ['seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'] }
+  ]
+};
